7a7640e346b6f0d73994256fa504997ec9628d85
Армен Мкртчян <MkrtchyanAS@outlook.com>
2017-12-27 22:17:29 +0500

    _

--- embebbedChromium/Form1.cs
+++ embebbedChromium/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace embebbedChromium
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser chromeBrowser;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeChromium();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            settings.BrowserSubprocessPath = @"x86\CefSharp.BrowserSubprocess.exe";
+            //settings.CefCommandLineArgs.Add("proxy-server", "94.177.250.204:443");
+            //settings.UserAgent = "My/Custom/User-Agent-AndStuff";
+            Cef.Initialize(settings);
+            chromeBrowser = new ChromiumWebBrowser("https://www.twitch.tv/fixzating");
+            Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
         }
     }
 }
